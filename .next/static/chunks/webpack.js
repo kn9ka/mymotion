57f7a96679ce153f6e42d10b0bdb46436ddc1e5e
Anton Krachkov <knyaka@Antons-MacBook-Pro.local>
2023-01-09 00:15:27 +0400

    fix: fix safari scroll bug

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "67d4ab1e477dd5b5"; }
+/******/ 		__webpack_require__.h = function() { return "802889132b8910c2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,11 +12,20 @@ const inter = Open_Sans({ subsets: ['latin'] });
 
 const isServer = typeof window === 'undefined';
 
+const setVH = () => {
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
 export default function Home() {
   useEffect(() => {
     if (!isServer) {
       require('../shared/lib/webgl');
+
+      window.addEventListener('resize', setVH);
     }
+    return () => window.removeEventListener('resize', setVH);
   }, []);
 
   const handleScroll = () => {
